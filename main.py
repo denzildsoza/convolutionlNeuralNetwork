96e3d05b12ff72018a4b611dd01cc21e4bad7048
denzildsoza <denzildsouza3661@gmail.com>
2024-11-04 11:36:10 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,60 @@
+from os.path  import join
+import struct
+from array import array
+import numpy as np
+import random
+import matplotlib.pyplot as plt
+from torch import nn,from_numpy
+
+class MnistDataloader(object):
+    def __init__(self, training_images_filepath,training_labels_filepath,
+                 test_images_filepath, test_labels_filepath):
+        self.training_images_filepath = training_images_filepath
+        self.training_labels_filepath = training_labels_filepath
+        self.test_images_filepath = test_images_filepath
+        self.test_labels_filepath = test_labels_filepath
+    
+    def read_images_labels(self, images_filepath, labels_filepath):        
+        labels = []
+        with open(labels_filepath, 'rb') as file:
+            magic, size = struct.unpack(">II", file.read(8))
+            if magic != 2049:
+                raise ValueError('Magic number mismatch, expected 2049, got {}'.format(magic))
+            labels = array("B", file.read())        
+        
+        with open(images_filepath, 'rb') as file:
+            magic, size, rows, cols = struct.unpack(">IIII", file.read(16))
+            if magic != 2051:
+                raise ValueError('Magic number mismatch, expected 2051, got {}'.format(magic))
+            image_data = array("B", file.read())        
+        images = []
+        for i in range(size):
+            images.append([0] * rows * cols)
+        for i in range(size):
+            img = np.array(image_data[i * rows * cols:(i + 1) * rows * cols])
+            img = img.reshape(28, 28)
+            images[i][:] = img            
+        
+        return images, labels
+            
+    def load_data(self):
+        x_train, y_train = self.read_images_labels(self.training_images_filepath, self.training_labels_filepath)
+        x_test, y_test = self.read_images_labels(self.test_images_filepath, self.test_labels_filepath)
+        return (x_train, y_train),(x_test, y_test)  
+
+# Set file paths based on added MNIST Datasets
+training_images_filepath = 'samples/train-images.idx3-ubyte'
+training_labels_filepath = 'samples/train-labels.idx1-ubyte'
+test_images_filepath = 'samples/t10k-images.idx3-ubyte'
+test_labels_filepath = 'samples/t10k-labels.idx1-ubyte'
+
+
+# Load MINST dataset
+print('Loading MNIST dataset...')
+mnist_dataloader = MnistDataloader(training_images_filepath, training_labels_filepath, test_images_filepath, test_labels_filepath)
+(x_train, y_train), (x_test, y_test) = mnist_dataloader.load_data()
+print('MNIST dataset loaded.')
+image = np.asarray(x_train[2])
+plt.imshow(image)
+# plt.show()
+
