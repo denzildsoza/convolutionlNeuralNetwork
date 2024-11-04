c9501774c9599d0116b11046f2333edc76b4d03a
denzildsoza <denzildsouza3661@gmail.com>
2024-11-04 19:49:22 +0530

    integrated cnn layers and forward function

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from array import array
 import numpy as np
 import random
 import matplotlib.pyplot as plt
-from torch import nn,from_numpy
+from torch import nn
+import torch
 
 class MnistDataloader(object):
     def __init__(self, training_images_filepath,training_labels_filepath,
@@ -54,7 +55,56 @@ print('Loading MNIST dataset...')
 mnist_dataloader = MnistDataloader(training_images_filepath, training_labels_filepath, test_images_filepath, test_labels_filepath)
 (x_train, y_train), (x_test, y_test) = mnist_dataloader.load_data()
 print('MNIST dataset loaded.')
-image = np.asarray(x_train[2])
-plt.imshow(image)
+# image = np.asarray(x_train[2])
+# plt.imshow(image)
 # plt.show()
 
+np_array = np.array(x_train[2])
+x_np = torch.from_numpy(np_array).reshape((1,1,28,28)).to(torch.float32)
+# conv1 = nn.Conv2d(in_channels=1,out_channels=28,kernel_size=5,    
+#                       stride=1,        
+#                       padding='same',      
+#             )
+# out = conv1(x_np)
+# plt.imshow(out.reshape((28,28)).detach().cpu().numpy())
+# plt.show()
+
+class ImageRecognize(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        
+        self.conv1 = nn.Sequential(nn.Conv2d(in_channels=1,out_channels=28,kernel_size=5,    
+                      stride=1,        
+                      padding='same',      
+            ),nn.ReLU())
+        self.pool1 = nn.MaxPool2d(padding=1,kernel_size=2)
+        
+        self.conv2 = nn.Sequential(nn.Conv2d(in_channels=28,out_channels=64,kernel_size=5,    
+                      stride=1,        
+                      padding='same',      
+            ),nn.ReLU())
+        self.relu = nn.ReLU()
+        self.flatten = nn.Flatten(start_dim=0)
+        
+        self.compcon1 = nn.Linear(in_features=4096,out_features=5000)
+        self.compcon2 = nn.Linear(in_features=5000,out_features=10)
+        
+        self.softmax = nn.Softmax(dim=0)
+        
+    def forward(self,x):
+        out = self.conv1(x)
+        out = self.pool1(out)
+        out = self.conv2(out)
+        out = self.pool1(out)
+        out = self.flatten(out)
+        out = self.compcon1(out)
+        out = self.compcon2(out)
+        out = self.relu(out)
+        out = self.softmax(out)
+        
+        return out
+
+model = ImageRecognize()
+print(model)
+out = model.forward(x_np)
+print(out)
