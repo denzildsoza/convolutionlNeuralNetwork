4bf983e59685f02006269b56486b855ee8dccdf2
denzildsoza <denzildsouza3661@gmail.com>
2024-11-06 11:23:08 +0530

    moved constants to constants file

---
+++ src/constants.py
@@ -0,0 +1,7 @@
+path = 'trainedweights/model_scripted.pt'
+
+# Set file paths based on added MNIST Datasets
+training_images_filepath = 'samples/train-images.idx3-ubyte'
+training_labels_filepath = 'samples/train-labels.idx1-ubyte'
+test_images_filepath = 'samples/t10k-images.idx3-ubyte'
+test_labels_filepath = 'samples/t10k-labels.idx1-ubyte'
